c31201e3ae80264a6f32686b898b8a4f634310c8
hanjy-slowturtle <h920915@gmail.com>
2022-08-25 22:42:11 +0900

    ~ #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  return state;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+console.log(countStore.getState());
+countStore.dispatch({ type: "ADD" });
+console.log(countStore.getState());
+countStore.dispatch({ type: "MINUS" });
 console.log(countStore.getState());
